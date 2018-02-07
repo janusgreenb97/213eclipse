64c5e4fb8a286cfd14c326c79c0858177d1aaa96
Ruoyang <rh522@scarletmail.rutgers.edu>
2018-02-06 20:34:28 -0500

    2_6_rh522_working on edit

--- src/songlib/app/songlib.java
+++ src/songlib/app/songlib.java
@@ -13,47 +13,62 @@ import songlib.model.Song;
 import songlib.view.songlibController;
 
 public class SongLib extends Application {
+	private Stage primaryStage;
 	/*
 	 * The data as an observable list of Songs.
 	 */
 	private ObservableList<Song> songData = FXCollections.observableArrayList();
-	 /**
-     * Constructor
-     */
-    public SongLib() {
-        // Add some sample data
-    	songData.add(new Song("Strawberry Field", "Betles"));
-    }
-    
-    /**
-     * Returns the data as an observable list of Persons. 
-     * @return
-     */
-    public ObservableList<Song> getSongData() {
-        return songData;
-    }
 
+	/**
+	 * Constructor
+	 */
+	public SongLib() {
+		// Add some sample data
+		
+		songData.add(new Song("Strawberry Field", "Betles"));
+		songData.add(new Song("222", "Betles"));
+		songData.add(new Song("333", "Betles"));
+		
+	}
+
+	/**
+	 * Returns the data as an observable list of Persons.
+	 * 
+	 * @return
+	 */
+	public ObservableList<Song> getSongData() {
+		return songData;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
-		AnchorPane songOverView = (AnchorPane) loader.load();
-		
-		 // Give the controller access to the main app.
-        songlibController controller = loader.getController();
-        controller.setMainApp(this);
-        
-		Scene scene = new Scene(songOverView);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("Song Library");
-		primaryStage.setResizable(false);
-		primaryStage.show();
-	} catch (IOException e) {
-		e.printStackTrace();
+			// load the songlib.fxml into songOverView
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
+			AnchorPane songOverView = (AnchorPane) loader.load();
+
+			// Give the controller access to the main app: SongLib.
+			songlibController controller = loader.getController();
+			controller.setMainApp(this);
+			
+			//set songOverView to be the scene
+			Scene scene = new Scene(songOverView);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Song Library");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+	 /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
 	public static void main(String[] args) {
 		launch(args);
--- src/songlib/view/songlibController.java
+++ src/songlib/view/songlibController.java
@@ -2,18 +2,20 @@ package songlib.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import songlib.app.SongLib;
 import songlib.model.Song;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import songlib.model.Song;
 
 public class songlibController {
-	@FXML
-	ListView<Song> listView;
+
 	@FXML
 	private TableView<Song> songTable;
 	@FXML
@@ -29,6 +31,12 @@ public class songlibController {
 	private Label albumLabel;
 	@FXML
 	private Label yearLabel;
+	@FXML
+	private TextField nameField;
+	@FXML
+	private Button confirmButton;
+	@FXML
+	private Button cancelButton;
 
 	// Reference to the main application.
 	private SongLib songlib;
@@ -40,22 +48,80 @@ public class songlibController {
 
 	@FXML
 	private void initialize() {
+
 		// Initialize the person table with the two columns.
 		nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
 		artistColumn.setCellValueFactory(cellData -> cellData.getValue().artistProperty());
+		showSongDetails(null);
+		songTable.getSelectionModel().selectedItemProperty()
+				.addListener((observable, oldValue, newValue) -> showSongDetails(newValue));
+
+		// TODO: when the app is first open, the first song need to be selected?
+
 	}
+
 	/**
-     * Is called by the main application to give a reference back to itself.
-     * 
-     * @param mainApp
-     */
-    public void setMainApp(SongLib songlib) {
-        this.songlib = songlib;
+	 * Is called by the main application to give a reference back to itself.
+	 * 
+	 * @param songLib
+	 */
+	public void setMainApp(SongLib songlib) {
+		this.songlib = songlib;
 
-        // Add observable list data to the table
-        songTable.setItems(songlib.getSongData());
-    }
-	public void start(Stage mainStage) {
+		// Add observable list data to the table
+		songTable.setItems(songlib.getSongData());
+
+		if (songTable.getItems().size() > 0) {
+
+			songTable.getSelectionModel().select(0);
+		}
+	}
+
+	public void showSongDetails(Song song) {
+		if (song != null) {
+			nameLabel.setText(song.getSongName());
+			artistLabel.setText(song.getArtist());
+			albumLabel.setText(song.getAlbum());
+			yearLabel.setText(Integer.toString(song.getYear()));
+		} else {
+			nameLabel.setText("");
+			artistLabel.setText("");
+			albumLabel.setText("");
+			yearLabel.setText("");
+		}
+	}
+
+	/**
+	 * Called when the user clicks on the delete button.
+	 */
+	@FXML
+	private void handleDeleteSong() {
+		int selectedIndex = songTable.getSelectionModel().getSelectedIndex();
+		if (selectedIndex >= 0) {
+			songTable.getItems().remove(selectedIndex);
+			songTable.getSelectionModel().select(selectedIndex);
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(songlib.getPrimaryStage());
+			alert.setTitle("No Selection");
+			alert.setHeaderText("No Song Selected");
+			alert.setContentText("Please select a song in the table.");
+			alert.showAndWait();
+		}
+	}
+
+	@FXML
+
+	private void handleEditSong() {
+		Song selectedSong = songTable.getSelectionModel().getSelectedItem();
+		confirmButton.setVisible(true);
+		cancelButton.setVisible(true);
+
+		nameLabel.setVisible(false);
+		nameField.setVisible(true);
+		nameField.setText(selectedSong.getSongName());
+		//TODO:alert
 
 	}
+
 }
